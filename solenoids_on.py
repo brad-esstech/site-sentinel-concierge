c3b086519178ae3c15fd135887ef097655a2f41a
brad-esstech <bbarton@esstech.com.au>
2025-08-10 20:24:45 +0800

    Update solenoids_on.py

--- solenoids_on.py
+++ solenoids_on.py
@@ -13,11 +13,11 @@ GPIO.setwarnings(False)
 # Check to see the current state of the GPIO out pin.
 # If not LOW, then we want to set it to LOW (relay closed)
 
-#if GPIO.input(config.egress_pin):
-  GPIO.output(config.egress_pin, GPIO.LOW) # relay closed
-
 #if GPIO.input(config.ingress_pin):
 #  GPIO.output(config.ingress_pin, GPIO.LOW) # relay closed
+
+#if GPIO.input(config.egress_pin):
+#  GPIO.output(config.egress_pin, GPIO.LOW) # relay closed
 
 #if GPIO.input(config.relay3_pin):
 #  GPIO.output(config.relay3_pin, GPIO.LOW) # relay closed
